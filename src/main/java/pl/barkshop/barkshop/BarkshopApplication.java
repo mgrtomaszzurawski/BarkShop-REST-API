0460d68ae715dfe5d8f77db9cb76cd9f53f7796f
Tomasz Zurawski <mgrtomaszzurawski@gmail.com>
2021-01-24 13:11:01 +0100

    First commit - Spring init

---
+++ src/main/java/pl/barkshop/barkshop/BarkshopApplication.java
@@ -0,0 +1,13 @@
+package pl.barkshop.barkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/barkshop/barkshop/BarkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.barkshop.barkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
